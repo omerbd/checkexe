e27b07ea86d3cb17d49da6917cf7e4ec6e564cf5
omerbd <omerbd@users.noreply.github.com>
2019-05-24 12:17:53 +0300

    remove drop_up.py

--- drop_up.py
+++
@@ -1,7 +0,0 @@
-import shutil
-PATH= input("please enter the path of the file you want to check:")
-try:
-	shutil.move(PATH, 'C:/Users/kodi/Dropbox/Apps/CheckExe/check.exe')
-	print ("OK")
-except Exception:
-	print ("ERROR")
